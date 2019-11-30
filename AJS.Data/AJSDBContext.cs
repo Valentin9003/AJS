5a32e7191ecf0d03f64d5438703f8bd77201574d
Valentin <Valio_180@abv.bg>
2019-11-30 22:39:39 +0200

    Refactoring the structure of AJS.Data

--- AJS.Data/AJSDBContext.cs
+++ AJS.Data/AJSDBContext.cs
@@ -2,7 +2,7 @@
 
 namespace AJS.Data
 {
-    public class Class1
+    public class AJSDbContext
     {
     }
 }
---
+++ AJS.Data/DataConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AJS.Data
+{
+    public static class DataConstants
+    {
+    }
+}
