f4b69290c469e4d980a886ec809bccc2709af0f0
Valentin <Valio_180@abv.bg>
2019-12-01 16:21:10 +0200

    Adding User Model

--- AJS.Data/Models/User.cs
+++ AJS.Data/Models/User.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AJS.Data.Models
 {
-   public class User
+   public class User: IdentityUser
     {
     }
 }
