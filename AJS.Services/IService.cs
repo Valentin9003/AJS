98648de542cdadb17f4c3378707fd5d653dad094
Valentin <Valio_180@abv.bg>
2019-12-01 16:56:02 +0200

    Adding method that gets called by the runtime services with reflection and creates instances

---
+++ AJS.Services/IService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AJS.Services
+{
+    public interface IService
+    {
+    }
+}
---
+++ AJS.Web/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AJS.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
+
+namespace AJS.Web.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// This method gets called by the runtime services with reflection and creates instances
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>        
+        public static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            Assembly
+                .GetAssembly(typeof(IService))
+                .GetTypes()
+                .Where(t => t.IsClass && t.GetInterfaces().Any(i => i.Name == $"I{t.Name}"))
+                .Select(t => new
+                {
+                    Interface = t.GetInterface($"I{t.Name}"),
+                    Implementation = t
+                })
+                .ToList()
+                .ForEach(s => services.AddTransient(s.Interface, s.Implementation));
+
+            return services;
+        }
+    }
+}
