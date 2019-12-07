2ab80e9dd351a8e431aaab2bf986addcf557aeba
Valentin <Valio_180@abv.bg>
2019-12-07 18:45:08 +0200

    Adding RequestLocalization ApplicationBuilder Extension

---
+++ AJS.Web/Infrastructure/Extensions/ApplicationBuilderExtentions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AJS.Web.Infrastructure.Extensions
+{
+    public static class ApplicationBuilderExtentions
+    {
+        public static IApplicationBuilder CongiguAndUseRequestLocalization(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var options = serviceScope
+                    .ServiceProvider
+                    .GetRequiredService<IOptions<RequestLocalizationOptions>>()
+                    .Value;
+
+                app.UseRequestLocalization(options);
+            }
+            return app;
+        }
+    }
+}
