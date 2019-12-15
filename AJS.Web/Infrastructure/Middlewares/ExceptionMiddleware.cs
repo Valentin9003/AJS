a8a88adbd4ad973998800d14789cc40f3788c48b
Valentin <Valio_180@abv.bg>
2019-12-15 22:33:26 +0200

    Add Exception Handler Middleware

---
+++ AJS.Web/Infrastructure/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using AJS.Web.Controllers;
+using AJS.Common.Logger;
+using AJS.Web.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AJS.Web.Infrastructure.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILoggerManager _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILoggerManager logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ProjectConstants.ErrorLogTitle} {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = ProjectConstants.ContentTypeJson;
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return context.Response.WriteAsync(new ErrorViewDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ProjectConstants.InternalServerErrorMessage
+            }.ToString());
+        }
+    }
+}
