742bb53d2e4ae12cc01ca519cc39309eeeb81e00
Valentin <Valio_180@abv.bg>
2019-12-16 00:30:02 +0200

    Add New Error Model

--- AJS.Web/Models/ErrorViewModel.cs
+++ AJS.Web/Models/ErrorViewModel.cs
@@ -1,11 +1,23 @@
+﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AJS.Web.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public string ErrorMessage { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string InformationMessage { get; set; }
+        public override string ToString()
+        {
+            return $"{StatusCode} {StatusMessage}" + Environment.NewLine
+                 + $"{ErrorMessage}" + Environment.NewLine +
+                   $"{InformationMessage}";
+        }
     }
 }
