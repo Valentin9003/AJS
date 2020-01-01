af9477c74f92c7e3356de5770628b97f94203bfc
Valentin <Valio_180@abv.bg>
2020-01-01 21:45:25 +0200

    Added AdCategory Data Model

---
+++ AJS.Data/Models/AdCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AJS.Data.Models
+{
+    /// <summary>
+    /// Ads Categories
+    /// </summary>
+   public class AdCategory
+    {
+        public string Name { get; set; } // Primary Key
+        public string Description { get; set; }
+        public string ParentCategoryId { get; set; } // Foreign Key for Parent Category
+
+        public AdCategory ParentCategory { get; set; }
+
+        public List<AdCategory> Categories = new List<AdCategory>();
+   }
+}
