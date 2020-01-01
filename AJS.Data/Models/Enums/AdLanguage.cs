2d4a8a44639bdeaa5497b0d4f11a985a019c0cea
Valentin <Valio_180@abv.bg>
2020-01-01 21:58:28 +0200

    Added AdLanguage Enum

---
+++ AJS.Data/Models/Enums/AdLanguage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AJS.Data.Models.Enums
+{
+    /// <summary>
+    /// Ad Language
+    /// </summary>
+    public enum AdLanguage
+    {
+        Undefined = 0,
+        EN = 1,
+        BG = 2,
+
+    }
+}
