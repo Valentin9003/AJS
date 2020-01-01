25f0c5c99102f739aa956a5024ef4fa88fddfecc
Valentin <Valio_180@abv.bg>
2020-01-01 21:44:11 +0200

    Added properties to User

--- AJS.Data/Models/User.cs
+++ AJS.Data/Models/User.cs
@@ -7,5 +7,10 @@ namespace AJS.Data.Models
 {
    public class User: IdentityUser
     {
+        public List<Ad> Ads { get; set; } = new List<Ad>(); // List of created Ads
+
+        // public List<Service> Services { get; set; } = new List<Service> // List of created Services
+
+        // public List<Jobs> Services { get; set; } = new List<Jobs> // List of created Jobs
     }
 }
