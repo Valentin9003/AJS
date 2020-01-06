b98ddab598064654fb60301ffea736c36915b91e
Valentin <Valio_180@abv.bg>
2020-01-06 20:19:32 +0200

    Add Ad Data Model

--- AJS.Data/Models/Ad.cs
+++ AJS.Data/Models/Ad.cs
@@ -9,13 +9,13 @@ namespace AJS.Data.Models
     /// <summary>
     /// Full Information About the Add
     /// </summary>
-   public class Ad
+    public class Ad
     {
         [Key]
         public string AdId { get; set; } // Primary Key
 
         [Required]
-        [StringLength(100,MinimumLength = 2)] // TODO: Add Multilingual Error Message
+        [StringLength(100, MinimumLength = 2)] // TODO: Add Multilingual Error Message
         public string Name { get; set; }
 
         [Required]
@@ -28,16 +28,16 @@ namespace AJS.Data.Models
 
         public AdCategory Category { get; set; }
 
-        public AdPicture MainPicture { get; set; } 
+        public AdPicture MainPicture { get; set; }
 
-        public List<AdPicture> Pictures { get; set;}  = new List<AdPicture>();
+        public List<AdPicture> Pictures { get; set; } = new List<AdPicture>();
 
         [Required]
         public AdLocation Location { get; set; }
 
         [Required]
         public AdDescription Description { get; set; }
-        
+
         [Required]
         public DateTime PublicationDate { get; set; }
 
