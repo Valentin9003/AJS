7516c1676004e2fccef39f61935e5412b0844f5b
Valentin <Valio_180@abv.bg>
2020-01-07 22:33:06 +0200

    Added  Data Constants for Data Annotation Attributes

--- AJS.Data/DataConstants.cs
+++ AJS.Data/DataConstants.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AJS.Data
+﻿namespace AJS.Data
 {
     public static class DataConstants
     {
+        #region String Length Data Annotation Constants
+        public const int DescriptionTextMnimumLength = 10;
+        public const int DescriptionTextMaximumLength = 700;
+        public const int AdNameMinimumLength = 2;
+        public const int AdNameMaximumLength = 22;
+        public const int AdLocationCountryMnimumLength = 3;
+        public const int AdLocationCountryMaximumLength = 15;
+        public const int AdLocationCityMnimumLength = 3;
+        public const int AdLocationCityMaximumLength = 15;
+        public const int AdLocationStreetMaximumLength = 15;
+        #endregion String Length Data Annotation Constants
     }
 }
