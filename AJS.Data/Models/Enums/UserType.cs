b4618825ce2e599f3b4bc751862dd5eca3fae457
Valentin <Valio_180@abv.bg>
2020-01-07 22:38:53 +0200

    Added UserType Enum

---
+++ AJS.Data/Models/Enums/UserType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AJS.Data.Models.Enums
+{
+    /// <summary>
+    /// Type of User: User or Company
+    /// </summary>
+    public enum UserType
+    {
+        Company = 0,
+        Person = 1
+    }
+}
