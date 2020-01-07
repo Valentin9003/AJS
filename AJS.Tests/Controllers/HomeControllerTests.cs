3b95d02bfa154b213d52b745ca48c5d26710fdb4
Valentin <Valio_180@abv.bg>
2020-01-07 20:10:52 +0200

    Add HomeControllerTests

--- AJS.Tests/Controllers/HomeControllerTests.cs
+++ AJS.Tests/Controllers/HomeControllerTests.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace AJS.Tests.Controllers
 {
    public class HomeControllerTests
     {
+        public HomeControllerTests()
+        {
+
+        }
+
+        //[Fact]
+        //public void SomeTest() 
+        //{
+        // TODO: Unit tests for Home Controller
+        //}
     }
 }
