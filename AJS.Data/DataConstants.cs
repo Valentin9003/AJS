6268074a3034cd2c006e1584fadfc9de5d43f741
Valentin <Valio_180@abv.bg>
2020-01-07 22:54:54 +0200

    Add Data Constants

--- AJS.Data/DataConstants.cs
+++ AJS.Data/DataConstants.cs
@@ -3,6 +3,7 @@
     public static class DataConstants
     {
         #region String Length Data Annotation Constants
+
         public const int DescriptionTextMnimumLength = 10;
         public const int DescriptionTextMaximumLength = 700;
         public const int AdNameMinimumLength = 2;
@@ -12,6 +13,9 @@
         public const int AdLocationCityMnimumLength = 3;
         public const int AdLocationCityMaximumLength = 15;
         public const int AdLocationStreetMaximumLength = 15;
+        public const int AdLocationAddressMaximumLength = 25;
+        public const int AdLocationPostCodeMaximumLength = 25; 
+
         #endregion String Length Data Annotation Constants
     }
 }
