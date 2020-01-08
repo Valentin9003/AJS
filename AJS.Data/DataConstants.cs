3df883f3c44be8b9698c455ffc34a29006dc2293
Valentin <Valio_180@abv.bg>
2020-01-08 21:35:16 +0200

    Added Data Annotations Constants

--- AJS.Data/DataConstants.cs
+++ AJS.Data/DataConstants.cs
@@ -3,17 +3,30 @@
     public static class DataConstants
     {
         #region String Length Data Annotation Constants
+
         public const int DescriptionTextMnimumLength = 10;
+
         public const int DescriptionTextMaximumLength = 700;
-        public const int AdNameMinimumLength = 2;
-        public const int AdNameMaximumLength = 22;
+
+        public const int AdTitleMinimumLength = 2;
+
+        public const int AdTitleMaximumLength = 22;
+
         public const int AdLocationCountryMnimumLength = 3;
+
         public const int AdLocationCountryMaximumLength = 15;
+
         public const int AdLocationCityMnimumLength = 3;
+
         public const int AdLocationCityMaximumLength = 15;
+
         public const int AdLocationStreetMaximumLength = 15;
+
         public const int AdLocationAddressMaximumLength = 15;
+
         public const int AdLocationPostCodeMaximumLength = 10;
+
+        public const int PictureByteArrayMaximumLength = 10 * 1024 * 1024;   // Max size for Picture is 10MB
 
         #endregion String Length Data Annotation Constants
     }
