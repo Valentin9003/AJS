a698f91924566f9fabeda6a66b7ceab75150bf39
Valentin <Valio_180@abv.bg>
2020-01-26 20:09:23 +0200

    Added localization in LoginWith2fa in InputModel

--- AJS.Web/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ AJS.Web/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -37,7 +37,7 @@ namespace AJS.Web.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(7, ErrorMessage = "The authenticator code must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Text)]
             [Display(Name = "Authenticator code")]
             public string TwoFactorCode { get; set; }
