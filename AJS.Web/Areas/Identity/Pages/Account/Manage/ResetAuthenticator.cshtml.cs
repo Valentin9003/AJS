1b7e14dfbb42152efe30296b1c8751d60d74fd37
Valentin <Valio_180@abv.bg>
2020-01-27 22:20:44 +0200

    Add Localization in ResetAuthenticatorModel

--- AJS.Web/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
+++ AJS.Web/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
@@ -6,6 +6,7 @@ using AJS.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 
 namespace AJS.Web.Areas.Identity.Pages.Account.Manage
@@ -15,15 +16,18 @@ namespace AJS.Web.Areas.Identity.Pages.Account.Manage
         UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         ILogger<ResetAuthenticatorModel> _logger;
+        private readonly IStringLocalizer<ResetAuthenticatorModel> _localizer;
 
         public ResetAuthenticatorModel(
             UserManager<User> userManager,
             SignInManager<User> signInManager,
-            ILogger<ResetAuthenticatorModel> logger)
+            ILogger<ResetAuthenticatorModel> logger,
+            IStringLocalizer<ResetAuthenticatorModel> localizer)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
+            _localizer = localizer;
         }
 
         [TempData]
@@ -34,7 +38,7 @@ namespace AJS.Web.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound(string.Format(_localizer[$"Unable to load user with ID '{0}'."], _userManager.GetUserId(User)));
             }
 
             return Page();
@@ -45,7 +49,7 @@ namespace AJS.Web.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound(string.Format(_localizer[$"Unable to load user with ID '{0}'."], _userManager.GetUserId(User)));
             }
 
             await _userManager.SetTwoFactorEnabledAsync(user, false);
@@ -53,7 +57,7 @@ namespace AJS.Web.Areas.Identity.Pages.Account.Manage
             _logger.LogInformation("User with ID '{UserId}' has reset their authentication app key.", user.Id);
 
             await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your authenticator app key has been reset, you will need to configure your authenticator app using the new key.";
+            StatusMessage = _localizer["Your authenticator app key has been reset, you will need to configure your authenticator app using the new key."];
 
             return RedirectToPage("./EnableAuthenticator");
         }
