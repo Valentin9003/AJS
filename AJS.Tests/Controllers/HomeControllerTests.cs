6fad5ba14f95baa1150aabffee4135e80a22bd38
Valentin Stoev <valio_180@abv.bg>
2020-02-02 20:33:47 +0200

    Add Unit Testing (#40)
    * Add XUnit Packages
    
    * Add HomeControllerTests Class
    
    * Add HomeControllerTests
    
    * Removed Unnecessary Folder
    
    * Add HomeController tests

---
+++ AJS.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AJS.Tests.Controllers
+{
+   public class HomeControllerTests
+    {
+        public HomeControllerTests()
+        {
+
+        }
+
+        //[Fact]
+        //public void SomeTest()
+        //{
+       
+        //}
+    }
+}
