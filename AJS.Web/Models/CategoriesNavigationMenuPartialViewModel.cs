8528cdef6fdbb5e6babcc541f98a8f4e58f31e8b
Valentin <Valio_180@abv.bg>
2020-02-05 21:22:56 +0200

    Added Categories Navigation Menu Partial View

---
+++ AJS.Web/Models/CategoriesNavigationMenuPartialViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AJS.Web.Models
+{
+    public class CategoriesNavigationMenuPartialViewModel
+    {
+        public string ParentCategoryId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        [Required]
+        public string Area { get; set; }
+
+        [Required]
+        public string Controller { get; set; }
+
+        [Required]
+        public string Action { get; set; }
+
+        [Required]
+        public string CategoryId { get; set; }
+
+        public List<CategoriesNavigationMenuPartialViewModel> SubCategories { get; set; }
+    }
+}
---
+++ AJS.Web/wwwroot/js/CategoriesNavigationMenuPartial.js
@@ -0,0 +1,7 @@
+﻿function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
