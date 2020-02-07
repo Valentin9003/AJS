7044a0cb0e10ac4afc02884eb41e4b7a1f07c6d0
Valentin <Valio_180@abv.bg>
2020-02-07 20:41:44 +0200

    Add API

---
+++ AJS.Web/Areas/Api/NewsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AJS.Web.Areas.Api
+{
+    [Route("api/News")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        public NewsController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
+        {
+            return new JsonResult("Implement me");
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public IActionResult GetById()
+        {
+            return new JsonResult("Implement me");
+        }
+
+        [HttpGet]
+        [Route("GetByCategory")]
+        public IActionResult GetByCategory()
+        {
+            return new JsonResult("Implement me");
+        }
+
+        [HttpGet]
+        [Route("GetByDate")]
+        public IActionResult GetByDate()
+        {
+            return new JsonResult("Implement me");
+        }
+
+        [HttpGet]
+        [Route("GetByLastDateSync")]
+        public IActionResult GetByLastDateSync()
+        {
+            return new JsonResult("Implement me");
+        }
+    }
+}
