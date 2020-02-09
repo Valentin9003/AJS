8cef6f219f5fd23b461de8ebb5899defb5ae382c
Valentin <Valio_180@abv.bg>
2020-02-09 19:04:04 +0200

    Add Model View Errors Messages Constants

--- AJS.Web/ProjectConstants.cs
+++ AJS.Web/ProjectConstants.cs
@@ -22,11 +22,11 @@
         public const string CategoryNavigationMenuPartialViewModelActionNameRequiredErrorMessage = "Action Name is Required";
         public const string CategoryNavigationMenuPartialViewModelCategoryNameRequiredErrorMessage = "Category Name is Required";
         public const string CategoryNavigationMenuPartialViewModelCategoryIdRequiredErrorMessage = "Category Id is Required";
-        public const string ChildCategoryNavigationMenuPartialViewModelAreaNameRequiredErrorMessage = "Area Name is Required";
-        public const string ChildCategoryNavigationMenuPartialViewModelControllerNameRequiredErrorMessage = "Controller Name is Required";
-        public const string ChildCategoryNavigationMenuPartialViewModelActionNameRequiredErrorMessage = "Action Name is Required";
-        public const string ChildCategoryNavigationMenuPartialViewModelCategoryNameRequiredErrorMessage = "Category Name is Required";
-        public const string ChildCategoryNavigationMenuPartialViewModelCategoryIdRequiredErrorMessage = "Category Id is Required";
+        public const string ChildCategoryNavigationMenuPartialViewModelAreaNameRequiredErrorMessage = "Child Area Name is Required";
+        public const string ChildCategoryNavigationMenuPartialViewModelControllerNameRequiredErrorMessage = "Child Controller Name is Required";
+        public const string ChildCategoryNavigationMenuPartialViewModelActionNameRequiredErrorMessage = "Child Action Name is Required";
+        public const string ChildCategoryNavigationMenuPartialViewModelCategoryNameRequiredErrorMessage = "Child Category Name is Required";
+        public const string ChildCategoryNavigationMenuPartialViewModelCategoryIdRequiredErrorMessage = "Child Category Id is Required";
 
         #endregion CategoryNavigationMenuPartialViewModel
     }
