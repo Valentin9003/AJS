1ff49a01a21ba475025edf16ef1bc2ac18a05c63
Valentin <Valio_180@abv.bg>
2020-02-11 21:48:27 +0200

    Localize AdCategory Table

--- AJS.Data/Migrations/20200211194429_LocalizeAdCategoryTable.Designer.cs
+++ AJS.Data/Migrations/20200211194429_LocalizeAdCategoryTable.Designer.cs
@@ -4,14 +4,16 @@ using AJS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AJS.Data.Migrations
 {
     [DbContext(typeof(AJSDbContext))]
-    partial class AJSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200211194429_LocalizeAdCategoryTable")]
+    partial class LocalizeAdCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AJS.Data/Models/AdCategoryTranslation.cs
@@ -0,0 +1,27 @@
+﻿using AJS.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AJS.Data.Models
+{
+    /// <summary>
+    /// Ad Category Translations
+    /// </summary>
+    public class AdCategoryTranslation
+    {
+        [Required]
+        public string AdCategoryTranslationId { get; set; }
+
+        [Required]
+        public string Translation { get; set; }
+
+        public AdCategoryLanguage Language { get; set; }
+
+        [Required]
+        public string CategoryId { get; set; }
+
+        public AdCategory Category { get; set; }
+    }
+}
